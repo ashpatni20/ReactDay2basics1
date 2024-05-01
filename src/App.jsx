9917f6a57df99c1847b2bd8d98e1fee48f0afaec
ashpatni20 <ash.patni20@gmail.com>
2024-05-01 22:22:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <h1>Topics covered</h1>
+      <p>
+        The following is a list of all topics we cover in the MDN learning area
+      </p>
+      <h4>Getting started with the web</h4>
+      <p>Provides a practical introduction to web development for complete beginners.</p>
+      <br />
+      <h4>HTML-Structuring the web</h4>
+      <p>HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail</p>
+      <br />
+      <h4>CSS-Styling the web</h4>
+      <p>CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.</p>
+
+    </div>
+  )
+}
+
+export default App
