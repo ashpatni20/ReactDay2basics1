be0882edc29079f00550e5af956fca237d679ff5
ashpatni20 <ash.patni20@gmail.com>
2024-05-01 22:28:07 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import './App.css'
 
 function App() {
   return(
-    <div>
+    <div className='mainDiv'>
       <h1>Topics covered</h1>
       <p>
         The following is a list of all topics we cover in the MDN learning area
       </p>
-      <h4>Getting started with the web</h4>
+      <h3>Getting started with the web</h3>
       <p>Provides a practical introduction to web development for complete beginners.</p>
       <br />
-      <h4>HTML-Structuring the web</h4>
+      <h3>HTML-Structuring the web</h3>
       <p>HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail</p>
       <br />
-      <h4>CSS-Styling the web</h4>
+      <h3>CSS-Styling the web</h3>
       <p>CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.</p>
 
     </div>
